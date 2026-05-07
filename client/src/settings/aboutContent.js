9fdbd5403eeea211467c32496201f91fd8382ee3
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2026-05-08 00:11:43 +0330

    Modify ABOUT_CONTENT with updated owner and links
    Updated various fields in ABOUT_CONTENT to reflect new information.

--- client/src/settings/aboutContent.js
+++ client/src/settings/aboutContent.js
@@ -1,6 +1,6 @@
 export const ABOUT_CONTENT = {
   copyright: {
-    ownerLabel: "@iPmartNetwork",
+    ownerLabel: "@iPmartNetwork1",
     ownerHref: "https://github.com/iPmartNetwork",
   },
   socials: [
@@ -10,11 +10,11 @@ export const ABOUT_CONTENT = {
     },
     {
       icon: "telegram",
-      href: "https://t.me/songbirdapp",
+      href: "https://t.me/BirdX",
     },
     {
       icon: "website",
-      href: "https://chat.pouyakhalili.ir/invite/c592b42783ba50a9833f6f4e15c5126a01b4f512242b5921",
+      href: "https://apps.ipmart.online",
     },
   ],
   supportIntro:
@@ -22,19 +22,19 @@ export const ABOUT_CONTENT = {
   wallets: [
     {
       label: "TRX",
-      address: "TPf1bEhipKpGkjo5N2Scj9nufNNh5TNrwX",
+      address: " ",
     },
     {
       label: "USDT (TRC20)",
-      address: "0x63313383611BbE11f4fEc139c14ad0b70281b822",
+      address: " ",
     },
     {
       label: "BTC",
-      address: "bc1q9hupvcc39juhf0k7rgzn6phn8s8jez365kzmuj",
+      address: " ",
     },
     {
       label: "TON",
-      address: "UQDzQ3xbWzKQvw8X8sWU82dksBeYqTHrT9sLzhBOyaESPjVy",
+      address: " ",
     },
   ],
 };
